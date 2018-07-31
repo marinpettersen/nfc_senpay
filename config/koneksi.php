8737b3c570476c5922c0b264cea34f16571c8d67
gins <anizsubhan@gmail.com>
2018-08-01 04:50:01 +0700

    config

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,16 @@
 <?php
+	// local connection
+	// $host = "127.0.0.1";
+	// $uname = "root";
+	// $password = '';
+	// $db = 'nfcpay';
 
-	$host = "127.0.0.1";
-	$uname = "root";
-	$password = '';
+	// $connect = mysqli_connect($host, $uname, $password, $db);
+
+	//remote conection
+	$host = "85.10.205.173:3306";
+	$uname = "marinpettersen";
+	$password = 'aniz1910';
 	$db = 'nfcpay';
 
 	$connect = mysqli_connect($host, $uname, $password, $db);
